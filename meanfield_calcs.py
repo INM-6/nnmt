2fbe8bada9632f9b6bb3f74f287599d2332c369a
Moritz Layer <m.layer@fz-juelich.de>
2018-09-13 12:28:13 +0200

    meanfield_calcs.py: implement calculation of mean

--- meanfield_calcs.py
+++ meanfield_calcs.py
@@ -3,12 +3,26 @@ from my_io import ureg
 import pint
 
 def firing_rates():
-    return np.arange(10) * ureg.meter
+    return np.arange(8) * ureg.Hz
 
-@ureg.wraps(ureg.meter, ureg.meter)
-def mean(firing_rates):
-    return firing_rates * 2
+# @ureg.wraps(ureg., ureg.Hz)
+def mean(nu, K, W, J, K_ext, nu_ext):
+    '''Returns vector of mean inputs to populations depending on
+    the firing rates nu of the populations.
 
-@ureg.wraps(ureg.meter**2, ureg.meter)
-def variance(firing_rates):
-    return np.power(firing_rates,  2)
+    Units as in Fourcoud & Brunel 2002, where current and potential
+    have the same units, unit of output array: pA*Hz
+    '''
+
+    # need to define this function, because np.dot is not compatible with pint
+    # quantities
+    @ureg.wraps(ureg.mV * ureg.Hz, (ureg.mV, ureg.Hz))
+    def dot(matrix, vector):
+        return np.dot(matrix, vector)
+
+    # contribution from within the network
+    m0 = dot(K*W, nu)
+    # contribution from external sources
+    m_ext = J*K_ext*nu_ext
+    m = m0+m_ext
+    return m
