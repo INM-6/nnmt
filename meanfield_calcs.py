650f3cee5233695696531c01f71cccf9f1a67fba
Moritz Layer <m.layer@fz-juelich.de>
2018-09-20 16:30:48 +0200

    meanfield_calcs.py: add documentation

--- meanfield_calcs.py
+++ meanfield_calcs.py
@@ -41,11 +41,13 @@ def firing_rates(dimension, tau_m, tau_s, tau_r, V_0_rel, V_th_rel, K, J, j,
     Quantity(np.ndarray, 'hertz')
         array of firing rates of each population in hertz
     '''
+
     rate_function = lambda mu, sigma: aux_calcs.nu0_fb433(tau_m, tau_s, tau_r,
                                                           V_th_rel, V_0_rel ,
                                                           mu, sigma)
 
     def get_rate_difference(nu):
+        """ calculate difference between new iteration step and previous one """
         mu = mean(nu, K, J, j, tau_m, nu_ext, K_ext)
         sigma = standard_deviation(nu, K, J, j, tau_m, nu_ext, K_ext)
         new_nu = np.array([x.magnitude for x in list(map(rate_function, mu,
@@ -67,16 +69,39 @@ def firing_rates(dimension, tau_m, tau_s, tau_r, V_0_rel, V_th_rel, K, J, j,
 @ureg.wraps(ureg.mV, (ureg.Hz, ureg.dimensionless, ureg.mV, ureg.mV,
                       ureg.s, ureg.Hz, ureg.dimensionless))
 def mean(nu, K, J, j, tau_m, nu_ext, K_ext):
-    '''Returns vector of mean inputs to populations depending on
-    the firing rates nu of the populations.
-
-    Units as in Fourcoud & Brunel 2002, where current and potential
-    have the same units, unit of output array: pA*Hz
     '''
+    Calc mean inputs to populations as function of firing rates of populations
+
+    Following Fourcaud & Brunel (2002)
+
+    Parameters:
+    -----------
+    nu: Quantity(np.ndarray, 'hertz')
+        firing rates of populations
+    K: Quantity(np.ndarray, 'dimensionless')
+        indegree matrix
+    J: Quantity(np.ndarray, 'millivolt')
+        effective connectivity matrix
+    j: Quantity(float, 'millivolt')
+        effective connectivity weight
+    tau_m: Quantity(float, 'millisecond')
+        membrane time constant
+    nu_ext: Quantity(float, 'hertz')
+        firing rate of external input
+    K_ext: Quantity(np.ndarray, 'dimensionless')
+        numbers of external input neurons to each population
+
+    Returns:
+    --------
+    Quantity(np.ndarray, 'millivolt')
+        array of mean inputs to each population in millivolt
+    '''
+
     # contribution from within the network
     m0 = np.dot(K * J, nu) * tau_m
     # contribution from external sources
     m_ext = j * K_ext * nu_ext * tau_m
+    # add them up
     m = m0 + m_ext
     return m
 
@@ -84,15 +109,38 @@ def mean(nu, K, J, j, tau_m, nu_ext, K_ext):
 @ureg.wraps(ureg.mV, (ureg.Hz, ureg.dimensionless, ureg.mV, ureg.mV, ureg.s,
                       ureg.Hz, ureg.dimensionless))
 def standard_deviation(nu, K, J, j, tau_m, nu_ext, K_ext):
-    '''Returns vector of standard_deviations of inputs to populations depending
-    on the firing rates f of the populations.
-    Units as in Fourcoud & Brunel 2002, where current and potential
-    have the same units, unit of output array: pA*Hz
     '''
-    # contribution from within the network
+    Calc standard devs of inputs to populations as function of firing rates
+
+    Following Fourcaud & Brunel (2002)
+
+    Parameters:
+    -----------
+    nu: Quantity(np.ndarray, 'hertz')
+        firing rates of populations
+    K: Quantity(np.ndarray, 'dimensionless')
+        indegree matrix
+    J: Quantity(np.ndarray, 'millivolt')
+        effective connectivity matrix
+    j: Quantity(float, 'millivolt')
+        effective connectivity weight
+    tau_m: Quantity(float, 'millisecond')
+        membrane time constant
+    nu_ext: Quantity(float, 'hertz')
+        firing rate of external input
+    K_ext: Quantity(np.ndarray, 'dimensionless')
+        numbers of external input neurons to each population
+
+    Returns:
+    --------
+    Quantity(np.ndarray, 'millivolt')
+        array of standard dev of inputs to each population in millivolt
+    '''
+    # contribution from within the network to variance
     var0 = np.dot(K * J**2, nu) * tau_m
-    # contribution from external sources
+    # contribution from external sources to variance
     var_ext = j**2 * K_ext * nu_ext * tau_m
+    # add them up
     var = var0 + var_ext
     # standard deviation is square root of variance
     std = np.sqrt(var)
