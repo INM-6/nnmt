dfc318f0a851eff13111fcfc828bc2d812a8ceaa
Moritz Layer <m.layer@fz-juelich.de>
2018-09-20 14:30:33 +0200

    minimal_usage_example.py: change such that its working with current implementation

--- minimal_usage_example.py
+++ minimal_usage_example.py
@@ -4,11 +4,14 @@ Currently it's not working."""
 import network as nw
 
 # instantiate network
-network = nw.Network(network_params='network_params.yaml', analysis_params='analysis_params.yaml', new_network_params={}, new_analysis_params={})
+network = nw.Network(network_params='network_params_microcircuit.yaml',
+                     analysis_params='analysis_params.yaml')
 
 # calculate working point
 working_point = network.working_point()
 
 # print results
 print('Working point:')
-print('mean: {}, variance: {}, firing rates: {}'.format(working_point['mu'], working_point['var'], working_point['rates']))
+print('mean: {}, std: {}, firing rates: {}'.format(working_point['mu'],
+                                                   working_point['sigma'],
+                                                   working_point['firing_rates']))
