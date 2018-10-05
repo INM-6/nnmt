6f0e986661b66c409e0a322336b4d36c2d82f514
Moritz Layer <m.layer@fz-juelich.de>
2018-10-05 16:15:45 +0200

    aux_calcs.py: extend documentation

--- aux_calcs.py
+++ aux_calcs.py
@@ -9,17 +9,40 @@ import mpmath
 
 from input_output import ureg
 
-# stationary firing rate of neuron with synaptic low-pass filter
-# of time constant tau_s driven by Gaussian noise with mean mu and
-# standard deviation sigma, from Fourcaud & Brunel 2002
+
 @ureg.wraps(ureg.Hz, (ureg.s, ureg.s, ureg.s, ureg.mV, ureg.mV, ureg.mV,
                       ureg.mV), strict=False)
 def nu0_fb433(tau_m, tau_s, tau_r, V_th_rel, V_0_rel, mu, sigma):
-    """Calculates stationary firing rates for exponential PSCs using
-    expression with taylor expansion in k = sqrt(tau_s/tau_m) (Eq. 433
-    in Fourcoud & Brunel 2002)
     """
+    Calcs stationary firing rates for exp PSCs
 
+    Calculates the stationary firing rate of a neuron with synaptic filter of
+    time constant tau_s driven by Gaussian noise with mean mu and standard
+    deviation sigma, using Eq. 433 in Fourcaud & Brunel (2002) with Taylor
+    expansion k = sqrt(tau_s/tau_m).
+
+    Parameters:
+    -----------
+    tau_m: float
+        Membrane time constant in seconds.
+    tau_s: float
+        Synaptic time constant in seconds.
+    tau_r: float
+        Refractory time in seconds.
+    V_th_rel: float
+        Relative threshold potential in mV.
+    V_0_rel: float
+        Relative reset potential in mV.
+    mu: float
+        Mean neuron activity in mV.
+    sigma:
+        Standard deviation of neuron activity in mV.
+
+    Returns:
+    --------
+    float:
+        Stationary firing rate in Hz.
+    """
     alpha = np.sqrt(2.) * abs(zetac(0.5) + 1)
     x_th = np.sqrt(2.) * (V_th_rel - mu) / sigma
     x_r = np.sqrt(2.) * (V_0_rel - mu) / sigma
@@ -36,23 +59,61 @@ def nu0_fb433(tau_m, tau_s, tau_r, V_th_rel, V_0_rel, mu, sigma):
         print(mu, sigma, x_th, x_r)
     return result
 
-def Phi(s):
-    return np.sqrt(np.pi / 2.) * (np.exp(s**2 / 2.) * (1 + erf(s / np.sqrt(2))))
-
-def Phi_prime_mu(s, sigma):
-    return -np.sqrt(np.pi) / sigma * (s * np.exp(s**2 / 2.)
-    * (1 + erf(s / np.sqrt(2)))
-    + np.sqrt(2) / np.sqrt(np.pi))
 
 def nu_0(tau_m, tau_r, V_th_rel, V_0_rel, mu, sigma):
-    """ Calculates stationary firing rates for delta shaped PSCs."""
+    """
+    Calculates stationary firing rates for delta shaped PSCs.
 
+    Parameters:
+    -----------
+    tau_m: float
+        Membrane time constant in seconds.
+    tau_r: float
+        Refractory time in seconds.
+    V_th_rel: float
+        Relative threshold potential in mV.
+    V_0_rel: float
+        Relative reset potential in mV.
+    mu: float
+        Mean neuron activity in mV.
+    sigma:
+        Standard deviation of neuron activity in mV.
+
+    Returns:
+    --------
+    float:
+        Stationary firing rate in Hz.
+    """
     if mu <= V_th_rel + (0.95 * abs(V_th_rel) - abs(V_th_rel)):
         return siegert1(tau_m, tau_r, V_th_rel, V_0_rel, mu, sigma)
     else:
         return siegert2(tau_m, tau_r, V_th_rel, V_0_rel, mu, sigma)
 
+
 def siegert1(tau_m, tau_r, V_th_rel, V_0_rel, mu, sigma):
+    """
+    Calculates stationary firing rates for delta shaped PSCs.
+
+    Parameters:
+    -----------
+    tau_m: float
+        Membrane time constant in seconds.
+    tau_r: float
+        Refractory time in seconds.
+    V_th_rel: float
+        Relative threshold potential in mV.
+    V_0_rel: float
+        Relative reset potential in mV.
+    mu: float
+        Mean neuron activity in mV.
+    sigma:
+        Standard deviation of neuron activity in mV.
+
+    Returns:
+    --------
+    float:
+        Stationary firing rate in Hz.
+    """
     # for mu < V_th_rel
     y_th = (V_th_rel - mu) / sigma
     y_r = (V_0_rel - mu) / sigma
@@ -86,7 +147,31 @@ def siegert1(tau_m, tau_r, V_th_rel, V_0_rel, mu, sigma):
 
     return out
 
+
 def siegert2(tau_m, tau_r, V_th_rel, V_0_rel, mu, sigma):
+    """
+    Calculates stationary firing rates for delta shaped PSCs.
+
+    Parameters:
+    -----------
+    tau_m: float
+        Membrane time constant in seconds.
+    tau_r: float
+        Refractory time in seconds.
+    V_th_rel: float
+        Relative threshold potential in mV.
+    V_0_rel: float
+        Relative reset potential in mV.
+    mu: float
+        Mean neuron activity in mV.
+    sigma:
+        Standard deviation of neuron activity in mV.
+
+    Returns:
+    --------
+    float:
+        Stationary firing rate in Hz.
+    """
     # for mu > V_th_rel
     y_th = (V_th_rel - mu) / sigma
     y_r = (V_0_rel - mu) / sigma
@@ -105,65 +190,132 @@ def siegert2(tau_m, tau_r, V_th_rel, V_0_rel, mu, sigma):
 
     return 1.0 / (tau_r + quad(integrand, 0.0, upper_bound)[0] * tau_m)
 
+
+def Phi(s):
+    """
+    where does this come from and what does it do???
+    """
+    return np.sqrt(np.pi / 2.) * (np.exp(s**2 / 2.) * (1 + erf(s / np.sqrt(2))))
+
+
+def Phi_prime_mu(s, sigma):
+    """
+    where does this come from and what does it do???
+    """
+    return -np.sqrt(np.pi) / sigma * (s * np.exp(s**2 / 2.)
+    * (1 + erf(s / np.sqrt(2)))
+    + np.sqrt(2) / np.sqrt(np.pi))
+
+
 @ureg.wraps(ureg.Hz*ureg.mV, (ureg.s, ureg.s, ureg.s, ureg.mV, ureg.mV, ureg.mV, ureg.mV), strict=False)
-def d_nu_d_mu_fb433(tau_m, tau_s, tau_r, V_th, V_0, mu, sigma):
+def d_nu_d_mu_fb433(tau_m, tau_s, tau_r, V_th_rel, V_0_rel, mu, sigma):
+    """
+    where does this come from and what does it do???
+
+    Parameters:
+    -----------
+    tau_m: float
+        Membrane time constant in seconds.
+    tau_s: float
+        Synaptic time constant in seconds.
+    tau_r: float
+        Refractory time in seconds.
+    V_th_rel: float
+        Relative threshold potential in mV.
+    V_0_rel: float
+        Relative reset potential in mV.
+    mu: float
+        Mean neuron activity in mV.
+    sigma:
+        Standard deviation of neuron activity in mV.
+
+    Returns:
+    --------
+    float:
+        Something in Hz/mV.
+    """
     alpha = np.sqrt(2) * abs(zetac(0.5) + 1)
-    x_th = np.sqrt(2) * (V_th - mu) / sigma
-    x_r = np.sqrt(2) * (V_0 - mu) / sigma
-    integral = 1. / (nu_0(tau_m, tau_r, V_th, V_0, mu, sigma) * tau_m)
+    x_th = np.sqrt(2) * (V_th_rel - mu) / sigma
+    x_r = np.sqrt(2) * (V_0_rel - mu) / sigma
+    integral = 1. / (nu_0(tau_m, tau_r, V_th_rel, V_0_rel, mu, sigma) * tau_m)
     prefactor = np.sqrt(tau_s / tau_m) * alpha / (tau_m * np.sqrt(2))
-    dnudmu = d_nu_d_mu(tau_m, tau_r, V_th, V_0, mu, sigma)
+    dnudmu = d_nu_d_mu(tau_m, tau_r, V_th_rel, V_0_rel, mu, sigma)
     dPhi_prime = Phi_prime_mu(x_th, sigma) - Phi_prime_mu(x_r, sigma)
     dPhi = Phi(x_th) - Phi(x_r)
     phi = dPhi_prime * integral + (2 * np.sqrt(2) / sigma) * dPhi**2
     return dnudmu - prefactor * phi / integral**3
 
-def d_nu_d_mu(tau_m, tau_r, V_th, V_0, mu, sigma):
-    y_th = (V_th - mu)/sigma
-    y_r = (V_0 - mu)/sigma
-    nu0 = nu_0(tau_m, tau_r, V_th, V_0, mu, sigma)
+
+def d_nu_d_mu(tau_m, tau_r, V_th_rel, V_0_rel, mu, sigma):
+    """
+    where does this come from and what does it do???
+
+    Parameters:
+    -----------
+    tau_m: float
+        Membrane time constant in seconds.
+    tau_s: float
+        Synaptic time constant in seconds.
+    tau_r: float
+        Refractory time in seconds.
+    V_th_rel: float
+        Relative threshold potential in mV.
+    V_0_rel: float
+        Relative reset potential in mV.
+    mu: float
+        Mean neuron activity in mV.
+    sigma:
+        Standard deviation of neuron activity in mV.
+
+    Returns:
+    --------
+    float:
+        Something in Hz/mV.
+    """
+    y_th = (V_th_rel - mu)/sigma
+    y_r = (V_0_rel - mu)/sigma
+    nu0 = nu_0(tau_m, tau_r, V_th_rel, V_0_rel, mu, sigma)
     return (np.sqrt(np.pi) * tau_m * nu0**2 / sigma
             * (np.exp(y_th**2) * (1 + erf(y_th)) - np.exp(y_r**2)
                * (1 + erf(y_r))))
 
-@ureg.wraps(ureg.dimensionless, (ureg.dimensionless, ureg.dimensionless), strict=False)
+
+@ureg.wraps(None, (ureg.dimensionless, ureg.dimensionless), strict=False)
 def Psi(z, x):
-   """
-   Calculates Psi(a,x) = exp(x**2/4)*U(a,x), where U(a,x) is the
-   parabolic cylinder function. Implementation uses the mpmath
-   functions. This is slower than the Fortran implementation `Psi`
-   and not used in this package but added for completeness.
-   """
+    """
+    Calcs Psi(z,x)=exp(x**2/4)*U(z,x), with U(z,x) the parabolic cylinder func.
+    """
+    return np.exp(0.25*x**2) * complex(mpmath.pcfu(z, -x))
 
-   value = np.exp(0.25*x**2) * complex(mpmath.pcfu(z, -x))
-   return value
-
-@ureg.wraps(ureg.dimensionless, (ureg.dimensionless, ureg.dimensionless), strict=False)
+@ureg.wraps(None, (ureg.dimensionless, ureg.dimensionless), strict=False)
 def d_Psi(z, x):
-   """
-   First derivative of Psi using recurrence relations (Eq.: 12.8.9
-   in http://dlmf.nist.gov/12.8)
-   """
+    """
+    First derivative of Psi using recurrence relations.
 
-   return (1. / 2. + z) * Psi(z + 1, x)
+    (Eq.: 12.8.9 in http://dlmf.nist.gov/12.8)
+    """
+    return (1. / 2. + z) * Psi(z + 1, x)
 
-@ureg.wraps(ureg.dimensionless, (ureg.dimensionless, ureg.dimensionless), strict=False)
+@ureg.wraps(None, (ureg.dimensionless, ureg.dimensionless), strict=False)
 def d_2_Psi(z, x):
-   """
-   Second derivative of Psi using recurrence relations (Eq.: 12.8.9
-   in http://dlmf.nist.gov/12.8)
-   """
+    """
+    Second derivative of Psi using recurrence relations.
 
-   return (1. / 2. + z) * (3. / 2. + z) * Psi(z + 2, x)
+    (Eq.: 12.8.9 in http://dlmf.nist.gov/12.8)
+    """
+    return (1. / 2. + z) * (3. / 2. + z) * Psi(z + 2, x)
 
-@ureg.wraps(ureg.dimensionless, (ureg.dimensionless, ureg.dimensionless, ureg.dimensionless), strict=False)
+@ureg.wraps(None, (ureg.dimensionless, ureg.dimensionless, ureg.dimensionless), strict=False)
 def Psi_x_r(z, x, y):
-   return Psi(z, x) - Psi(z, y)
+    """Difference of Psi for same first argument z."""
+    return Psi(z, x) - Psi(z, y)
 
-@ureg.wraps(ureg.dimensionless, (ureg.dimensionless, ureg.dimensionless, ureg.dimensionless), strict=False)
+@ureg.wraps(None, (ureg.dimensionless, ureg.dimensionless, ureg.dimensionless), strict=False)
 def dPsi_x_r(z, x, y):
-   return d_Psi(z, x) - d_Psi(z, y)
+    """Difference of derivatives of Psi for same first argument z."""
+    return d_Psi(z, x) - d_Psi(z, y)
 
-@ureg.wraps(ureg.dimensionless, (ureg.dimensionless, ureg.dimensionless, ureg.dimensionless), strict=False)
+@ureg.wraps(None, (ureg.dimensionless, ureg.dimensionless, ureg.dimensionless), strict=False)
 def d2Psi_x_r(z, x, y):
-   return d_2_Psi(z, x) - d_2_Psi(z, y)
+    """Difference of second derivatives of Psi for same first argument z."""
+    return d_2_Psi(z, x) - d_2_Psi(z, y)
