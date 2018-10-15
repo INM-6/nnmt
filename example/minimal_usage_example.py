c68ab6d54932df0231156d33e908da8429e76ae3
Johanna Senk <j.senk@fz-juelich.de>
2018-10-15 16:58:30 +0200

    Add minimal usage example

---
+++ example/minimal_usage_example.py
@@ -0,0 +1,15 @@
+# Minimal working example
+
+import lif_meanfield_tools as lmt
+
+network = lmt.Network(network_params='network_params_microcircuit.yaml',
+                      analysis_params='analysis_params.yaml')
+
+# calculate working point
+working_point = network.working_point()
+
+# print results
+print('Working point:')
+print('mean: {}'.format(working_point['mu']))
+print('std: {}'.format(working_point['sigma']))
+print('firing rates: {}'.format(working_point['firing_rates']))
