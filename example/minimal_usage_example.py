338ef7ad73e6d74a440f8001eeab6d278b65a9de
Moritz Layer <m.layer@fz-juelich.de>
2018-10-17 09:29:45 +0200

    minimal_usage_example.py: needed to change import of ureg

--- example/minimal_usage_example.py
+++ example/minimal_usage_example.py
@@ -1,7 +1,7 @@
 # Minimal working example
 
 import lif_meanfield_tools as lmt
-from lif_meanfield_tools import ureg
+from lif_meanfield_tools.__init__ import ureg
 
 # instantiate network
 network = lmt.Network(network_params='network_params_microcircuit.yaml',
@@ -15,6 +15,9 @@ print('Working point:')
 print('mean: {}'.format(working_point['mu']))
 print('std: {}'.format(working_point['sigma']))
 print('firing rates: {}'.format(working_point['firing_rates']))
+
+# print(network.transfer_function())
+print(network.transfer_function(10*ureg.Hz))
 
 # save results
 network.save()
