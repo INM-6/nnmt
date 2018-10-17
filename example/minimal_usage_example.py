84e3ebc64c54a61e849256c90694aa65be402a8e
Moritz Layer <m.layer@fz-juelich.de>
2018-10-17 09:00:00 +0200

    minimal_usage_example.py: import ureg and add documentaion

--- example/minimal_usage_example.py
+++ example/minimal_usage_example.py
@@ -1,15 +1,20 @@
 # Minimal working example
 
 import lif_meanfield_tools as lmt
+from lif_meanfield_tools import ureg
 
+# instantiate network
 network = lmt.Network(network_params='network_params_microcircuit.yaml',
                       analysis_params='analysis_params.yaml')
 
-# calculate working point
+# # calculate working point
 working_point = network.working_point()
 
-# print results
+#print results
 print('Working point:')
 print('mean: {}'.format(working_point['mu']))
 print('std: {}'.format(working_point['sigma']))
 print('firing rates: {}'.format(working_point['firing_rates']))
+
+# save results
+network.save()
