7de765d4105b63c62f5fdf8606967cfb637721c6
Johanna Senk <j.senk@fz-juelich.de>
2019-05-09 13:53:21 +0200

    Add minimal example scripts for power spectrum and sensitivity measure

---
+++ example/test_power_spectrum.py
@@ -0,0 +1,25 @@
+# Minimal working example to test power spectra
+
+import lif_meanfield_tools as lmt
+from lif_meanfield_tools.__init__ import ureg
+import numpy as np
+import matplotlib.pyplot as plt
+
+# instantiate network
+network = lmt.Network(network_params='network_params_microcircuit.yaml',
+                      analysis_params='analysis_params.yaml')
+
+network.transfer_function()
+network.save()
+
+freqs = network.analysis_params['omegas']/2./np.pi
+power = network.power_spectra()
+network.save()
+
+plt.figure()
+for i in range(8):
+    plt.plot(freqs, np.sqrt(power[i]), label=network.network_params['populations'][i])
+plt.yscale('log')
+plt.legend()
+
+plt.show()
