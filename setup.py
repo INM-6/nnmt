7cb233201ca8fb19d59e4a6f71bcd6626f5541a4
Johanna Senk <j.senk@fz-juelich.de>
2020-02-04 13:44:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,24 @@
 from setuptools import setup
 
-long_description = open("README.md").read()
-
-setup(name='lif_meanfield_tools',
-      version='0.1',
-      description='Mean-field toolbox for networks of LIF neurons.',
-      url='https://github.com/INM-6/lif_meanfield_tools',
-      author='Moritz Layer, Hannah Bos, Jannis Schuecker, Johanna Senk, Karolina Korvasova, Moritz Helias',
-      author_email='m.layer@fz-juelich.de',
-      license='MIT',
-      packages=['lif_meanfield_tools'],
+setup(name = 'lif_meanfield_tools',
+      version = '0.1',
+      description = 'Mean-field toolbox for networks of LIF neurons.',
+      long_description = open('README.md').read(),
+      url = 'https://github.com/INM-6/lif_meanfield_tools',
+      author = 'see doc/authors.rst',
+      author_email = 'm.layer@fz-juelich.de',
+      license = 'GNU GPLv3',
+      packages = ['lif_meanfield_tools'],
       install_requires = [
         'setuptools>=23.1.0',
         'numpy>=1.8',
         'scipy>=0.14',
         'Cython>=0.20',
         'h5py>=2.5',
-        'pint',
+        'pint==0.9',
         'h5py_wrapper',
         'pyyaml',
         'requests',
         'mpmath',
-        'decorator',
-])
-
+        'decorator'],
+     python_requires='>=3')
