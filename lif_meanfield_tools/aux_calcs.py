bac6016742de4746d75e540bee1486f497c94d26
Simon Essink <s.essink@fz-juelich.de>
2020-02-06 22:48:56 +0100

    Fix ambigous use of Phi and insert sources/explanation (draft)
    - Phi() as it was in the package referred to a helper function used for 
    stationary firing rate calculation
    - Phi() however was also used as helper function for the truncated 
    Gaussian distribution, which looks a bit different -> added 
    truncated_gaussian_helper (better rename!)

--- lif_meanfield_tools/aux_calcs.py
+++ lif_meanfield_tools/aux_calcs.py
@@ -116,7 +116,7 @@ def nu_0(tau_m, tau_r, V_th_rel, V_0_rel, mu, sigma):
 def nu0_fb(tau_m, tau_s, tau_r, V_th, V_r, mu, sigma):
     """
     Calculates stationary firing rates for filtered synapses based on
-    Fourcaud & Brunel 2002.
+    Fourcaud & Brunel 2002 (using the shift of the integration boundaries)
 
     Parameters:
     -----------
@@ -253,16 +253,22 @@ def siegert2(tau_m, tau_r, V_th_rel, V_0_rel, mu, sigma):
 
 def Phi(s):
     """
-    where does this come from and what does it do???
-    """
-    # return np.sqrt(np.pi / 2.) * (np.exp(s**2 / 2.) * (1 + erf(s / np.sqrt(2))))
-    return 0.5 * (1 + erf(s / np.sqrt(2)))
+    helper function to calculate stationary firing rates with synaptic
+    filtering
 
+    corresponds to u^-2 F in Eq. 53 of the following publication
+
+
+    Schuecker, J., Diesmann, M. & Helias, M.
+    Reduction of colored noise in excitable systems to white
+    noise and dynamic boundary conditions. 1–23 (2014).
+    """
+    return np.sqrt(np.pi / 2.) * (np.exp(s**2 / 2.) * (1 + erf(s / np.sqrt(2))))
 
 
 def Phi_prime_mu(s, sigma):
     """
-    where does this come from and what does it do???
+    Derivative of the helper function Phi(s) with respect to the mean input
     """
     return -np.sqrt(np.pi) / sigma * (s * np.exp(s**2 / 2.)
     * (1 + erf(s / np.sqrt(2)))
@@ -272,7 +278,13 @@ def Phi_prime_mu(s, sigma):
            (ureg.s, ureg.s, ureg.s, ureg.mV, ureg.mV, ureg.mV, ureg.mV))
 def d_nu_d_mu_fb433(tau_m, tau_s, tau_r, V_th_rel, V_0_rel, mu, sigma):
     """
-    where does this come from and what does it do???
+    Derivative of the stationary firing rates with synaptic filtering
+    with respect to the mean input
+
+    See Appendix B in
+    Schuecker, J., Diesmann, M. & Helias, M.
+    Reduction of colored noise in excitable systems to white
+    noise and dynamic boundary conditions. 1–23 (2014).
 
     Parameters:
     -----------
@@ -310,7 +322,8 @@ def d_nu_d_mu_fb433(tau_m, tau_s, tau_r, V_th_rel, V_0_rel, mu, sigma):
 
 def d_nu_d_mu(tau_m, tau_r, V_th_rel, V_0_rel, mu, sigma):
     """
-    where does this come from and what does it do???
+    Derivative of the stationary firing rate without synaptic filtering
+    with respect to the mean input
 
     Parameters:
     -----------
@@ -433,6 +446,13 @@ def d_nu_d_nu_in_fb(tau_m, tau_s, tau_r, V_th, V_r, j, mu, sigma):
              0.5 * y_th * j / sigma - np.exp(y_r_fb**2) * (1 + erf(y_r_fb)) * 0.5 * y_r * j / sigma)
 
     return lin + sqr, lin, sqr
+
+
+def truncated_gaussian_helper(s):
+    """
+    see Bos 2015 Eq. 15
+    """
+    return 0.5 * (1 + erf(s / np.sqrt(2)))
 
 
 def determinant(matrix):
