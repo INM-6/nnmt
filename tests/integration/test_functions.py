1729972ca688001576d8d5369667f95ac3ac1f7a
Moritz Layer <m.layer@fz-juelich.de>
2020-05-26 17:44:43 +0200

    Collect tests for integration suite

---
+++ tests/integration/test_functions.py
@@ -0,0 +1,43 @@
+class Test_Network_functions:
+    
+    def test_firing_rates(self):
+        pass
+    
+    def test_mean_input(self):
+        pass
+    
+    def test_standard_deviation(self):
+        pass
+    
+    def test_working_point(self):
+        pass
+    
+    def test_delay_dist_matrix(self):
+        pass
+    
+    def test_delay_dist_matrix_single(self):
+        pass
+    
+    def test_transfer_function(self):
+        pass
+    
+    def test_transfer_function_single(self):
+        pass
+    
+    def test_sensitivity_measure(self):
+        pass
+    
+    def test_power_spectra(self):
+        pass
+    
+    def test_eigenvalue_spectra(self):
+        pass
+    
+    def test_r_eigenvec_spectra(self):
+        pass
+    
+    def test_l_eigenvec_spectra(self):
+        pass
+    
+    def test_additional_rates_for_fixed_input(self):
+        pass
