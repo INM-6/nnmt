480f907e5c1610352c4f5c092353ff1535fb7f69
Alexander van Meegen <a.van.meegen@fz-juelich.de>
2021-05-20 21:25:33 +0200

    Bring Sanzeni example to v1.0

--- examples/sanzeni.py
+++ examples/sanzeni.py
@@ -1,118 +1,107 @@
 import numpy as np
 import matplotlib.gridspec as gridspec
 
-from lif_meanfield_tools import ureg
-from lif_meanfield_tools.meanfield_calcs import firing_rates
+from lif_meanfield_tools.lif.delta import _firing_rates
 
 import matplotlib.pyplot as plt
 plt.style.use('frontiers.mplstyle')
 
 
 # ===== parameters =====
-nu_ext_steps = 50  # number of external rates (x-axis)
 # common parameters for all plots
 params_all = dict(
-    dimension=2,  # E and I
-    # time constants
-    tau_m=20*ureg.ms, tau_s=0*ureg.ms, tau_r=2*ureg.ms,
-    # reset and threshold voltage relative to leak
-    V_0_rel=10*ureg.mV, V_th_rel=20*ureg.mV,
-    # no additional external input
-    g=1, nu_e_ext=0*ureg.Hz, nu_i_ext=0*ureg.Hz,
-    # method irrelevant because tau_s=0
-    method='shift'
+    # time constants in s
+    tau_m=20.*1e-3, tau_r=2.*1e-3, tau_m_ext=20.*1e-3,
+    # reset and threshold voltage relative to leak in mV
+    V_0_rel=10., V_th_rel=20.,
 )
 # Saturation-driven nonlinearity
 g_E, g_I, a_E, a_I = 8, 7, 4, 2
 params_sdn = dict(
-    J=np.array([[0.2, -g_E*0.2], [0.2, -g_I*0.2]])*ureg.mV,
+    J=np.array([[0.2, -g_E*0.2], [0.2, -g_I*0.2]]),
     K=np.array([[400., 100.], [400., 100.]]),
-    j=np.array([0.2, 0.2])*ureg.mV,
+    J_ext=np.array([0.2, 0.2]),
     K_ext=np.array([a_E*400., a_I*400.])
 )
 # Saturation-driven multisolution
 g_E, g_I, a_E, a_I = 2.08, 1.67, 1, 1
 params_sdm = dict(
-    J=np.array([[0.2, -g_E*0.2], [2.4*0.2/2.5, -g_I*2.4*0.2/2.5]])*ureg.mV,
+    J=np.array([[0.2, -g_E*0.2], [2.4*0.2/2.5, -g_I*2.4*0.2/2.5]]),
     K=np.array([[400., 100.], [400., 100.]]),
-    j=np.array([0.2, 2.4*0.2/2.5])*ureg.mV,
+    J_ext=np.array([0.2, 2.4*0.2/2.5]),
     K_ext=np.array([a_E*400., a_I*400.])
 )
 # Response-onset supersaturation
 g_E, g_I, a_E, a_I = 4.5, 2.9, 1, 1
 params_ros = dict(
-    J=np.array([[0.2, -g_E*0.2], [0.2, -g_I*0.2]])*ureg.mV,
+    J=np.array([[0.2, -g_E*0.2], [0.2, -g_I*0.2]]),
     K=np.array([[400., 100.], [400., 100.]]),
-    j=np.array([0.2, 0.2])*ureg.mV,
+    J_ext=np.array([0.2, 0.2]),
     K_ext=np.array([a_E*400., a_I*400.])
 )
 # Mean-driven multisolution
 g_E, g_I, a_E, a_I = 4.1, 2.46, 1, 0.2
 params_mdm = dict(
     K=np.array([[800., 200.], [400., 100.]]),
-    J=np.array([[0.2, -g_E*0.2], [0.2, -g_I*0.2]])*ureg.mV,
-    j=np.array([0.2, 0.2])*ureg.mV,
+    J=np.array([[0.2, -g_E*0.2], [0.2, -g_I*0.2]]),
+    J_ext=np.array([0.2, 0.2]),
     K_ext=np.array([a_E*800., a_I*400.])
 )
 # Noise-driven multisolution
 g_E, g_I, a_E, a_I = 7, 6, 1, 0.7
 params_ndm = dict(
     K=np.array([[400., 100.], [400., 100.]]),
-    J=np.array([[0.5, -g_E*0.5], [0.5, -g_I*0.5]])*ureg.mV,
-    j=np.array([0.5, 0.5])*ureg.mV, K_ext=np.array([a_E*400., a_I*400.])
+    J=np.array([[0.5, -g_E*0.5], [0.5, -g_I*0.5]]),
+    J_ext=np.array([0.5, 0.5]),
+    K_ext=np.array([a_E*400., a_I*400.])
 )
 
 
 # ===== self-consistent rates =====
 def solve_theory(params, nu_0, nu_ext_min, nu_ext_max, nu_ext_steps,
-                 fp_iteration=True, print_rates=False):
+                 method='ODE', print_rates=False):
     params.update(params_all)
-    nu_ext_arr = np.linspace(nu_ext_min, nu_ext_max, nu_ext_steps)*ureg.Hz
-    nu_arr = np.zeros((nu_ext_steps+1, 2))*ureg.Hz
+    nu_ext_arr = np.linspace(nu_ext_min, nu_ext_max, nu_ext_steps)
+    nu_arr = np.zeros((nu_ext_steps, 2))
     for i, nu_ext in enumerate(nu_ext_arr):
-        nu_arr[i+1] = firing_rates(nu_0=nu_0*ureg.Hz, nu_ext=nu_ext,
-                                   fp_iteration=fp_iteration, **params)
+        try:
+            nu_arr[i] = _firing_rates(nu_0=nu_0, nu_ext=nu_ext,
+                                      fixpoint_method=method, **params)
+        except RuntimeError:
+            # set non-convergent solutions to nan
+            nu_arr[i] = (np.nan, np.nan)
         if print_rates:
-            print(nu_ext, nu_arr[i+1])
-    nu_ext_arr = np.insert(nu_ext_arr, 0, 0)
+            print(nu_ext, nu_arr[i])
     return nu_ext_arr, nu_arr
 
 
 print('Calculating self-consitent rates...')
-# TODO: optimize intervals & find more unstable branches
 print('Saturation-driven nonlinearity...')
-nu_ext_sdn, nu_sdn = solve_theory(params_sdn, (0, 0), 1, 100,
-                                  nu_ext_steps)
+nu_ext_sdn, nu_sdn = solve_theory(params_sdn, (0, 0), 1, 100, 50)
 print('Saturation-driven multisolution...')
-nu_ext_sdm_a, nu_sdm_a = solve_theory(params_sdm, (0, 0), 1, 9,
-                                      nu_ext_steps)
-nu_ext_sdm_b, nu_sdm_b = solve_theory(params_sdm, (500, 500), 1, 100,
-                                      nu_ext_steps)
-nu_ext_sdm_c, nu_sdm_c = solve_theory(params_sdm, (10, 10), 1, 20,
-                                      nu_ext_steps, fp_iteration=False)
-nu_ext_sdm_d, nu_sdm_d = solve_theory(params_sdm, (100, 100), 1, 20,
-                                      nu_ext_steps, fp_iteration=False)
+nu_ext_sdm_a, nu_sdm_a = solve_theory(params_sdm, (0, 0), 1, 9, 10)
+nu_ext_sdm_b, nu_sdm_b = solve_theory(params_sdm, (500, 500), 1, 100, 50)
+nu_ext_sdm_c, nu_sdm_c = solve_theory(params_sdm, (10, 10), 1, 20, 10,
+                                      method='LSTSQ')
+nu_ext_sdm_d, nu_sdm_d = solve_theory(params_sdm, (100, 100), 1, 20, 10,
+                                      method='LSTSQ')
 print('Response-onset supersaturation...')
-nu_ext_ros_a, nu_ros_a = solve_theory(params_ros, (0, 0), 0.5, 50,
-                                      nu_ext_steps)
-nu_ext_ros_b, nu_ros_b = solve_theory(params_ros, (10, 10), 7.5, 12.5,
-                                      nu_ext_steps)
+nu_ext_ros_a, nu_ros_a = solve_theory(params_ros, (0, 0), 0.5, 50, 50)
+nu_ext_ros_b, nu_ros_b = solve_theory(params_ros, (10, 10), 7.5, 12.5, 50)
 print('Mean-driven multisolution...')
-nu_ext_mdm_a, nu_mdm_a = solve_theory(params_mdm, (0, 0), 0.1, 5,
-                                      nu_ext_steps, fp_iteration=False)
-nu_ext_mdm_b, nu_mdm_b = solve_theory(params_mdm, (50, 50), 0.1, 10,
-                                      nu_ext_steps, fp_iteration=False)
-nu_ext_mdm_c, nu_mdm_c = solve_theory(params_mdm, (10, 0), 0.1, 5,
-                                      nu_ext_steps, fp_iteration=False)
+nu_ext_mdm_a, nu_mdm_a = solve_theory(params_mdm, (0, 0), 0.1, 5, 25,
+                                      method='LSTSQ')
+nu_ext_mdm_b, nu_mdm_b = solve_theory(params_mdm, (50, 50), 0.1, 10, 50,
+                                      method='LSTSQ')
+nu_ext_mdm_c, nu_mdm_c = solve_theory(params_mdm, (10, 0), 0.1, 5, 25,
+                                      method='LSTSQ')
 print('Noise-driven multisolution...')
-nu_ext_ndm_a, nu_ndm_a = solve_theory(params_ndm, (0, 0), 0.05, 5,
-                                      nu_ext_steps)
-nu_ext_ndm_b, nu_ndm_b = solve_theory(params_ndm, (10, 10), 0.05, 5,
-                                      nu_ext_steps)
-nu_ext_ndm_c, nu_ndm_c = solve_theory(params_ndm, (5, 4), 0.05, 5,
-                                      nu_ext_steps, fp_iteration=False)
-nu_ext_ndm_d, nu_ndm_d = solve_theory(params_ndm, (2, 0), 0.05, 5,
-                                      nu_ext_steps, fp_iteration=False)
+nu_ext_ndm_a, nu_ndm_a = solve_theory(params_ndm, (0, 0), 0.05, 5, 50)
+nu_ext_ndm_b, nu_ndm_b = solve_theory(params_ndm, (10, 10), 0.05, 5, 50)
+nu_ext_ndm_c, nu_ndm_c = solve_theory(params_ndm, (5, 4), 0.05, 5, 50,
+                                      method='LSTSQ')
+nu_ext_ndm_d, nu_ndm_d = solve_theory(params_ndm, (2, 0), 0.05, 5, 50,
+                                      method='LSTSQ')
 
 
 # ===== figure =====
@@ -132,39 +121,44 @@ def plot_rates(ax, nu_ext_arr_lst, nu_arr_lst, xmax, ymax, xlabel, ylabel,
 
 
 print('Plotting...')
-fig = plt.figure(figsize=(7.08661, 1.45),  # two column figure, 180mm wide
+fig = plt.figure(figsize=(3.34646, 3.34646),  # two column figure, 180mm wide
                  constrained_layout=True)
-gs = gridspec.GridSpec(1, 5, figure=fig)
-label_prms = dict(x=-0.3, y=1.3, fontsize=10, fontweight='bold',
+gs = gridspec.GridSpec(3, 2, figure=fig)
+label_prms = dict(x=-0.3, y=1.4, fontsize=10, fontweight='bold',
                   va='top', ha='right')
 colors = ['#c44e52', '#4c72b0']
+# Sketch
+ax = fig.add_subplot(gs[0, 0])
+ax.axis('off')
+ax.set_title('Network')
+ax.text(s='(A)', transform=ax.transAxes, **label_prms)
 # Saturation-driven nonlinearity
-plot_rates(fig.add_subplot(gs[0, 0]), [nu_ext_sdn], [nu_sdn], 100, 500,
-           r'$\nu_X$ [spks/s]', r'$\nu$ [spks/s]', 'SDN',
-           colors, '(A)', label_prms)
+plot_rates(fig.add_subplot(gs[0, 1]), [nu_ext_sdn], [nu_sdn], 100, 500,
+           '', r'$\nu$ [spks/s]', 'SDN',
+           colors, '(B)', label_prms)
 # Saturation-driven multisolution
-plot_rates(fig.add_subplot(gs[0, 1]),
+plot_rates(fig.add_subplot(gs[1, 0]),
            [nu_ext_sdm_a, nu_ext_sdm_b, nu_ext_sdm_c, nu_ext_sdm_d],
            [nu_sdm_a, nu_sdm_b, nu_sdm_c, nu_sdm_d], 100, 500,
-           r'$\nu_X$ [spks/s]', '', 'SDM',
-           colors, '(B)', label_prms)
-# Response-onset supersaturation
-plot_rates(fig.add_subplot(gs[0, 2]), [nu_ext_ros_a, nu_ext_ros_b],
-           [nu_ros_a, nu_ros_b], 50, 5,
-           r'$\nu_X$ [spks/s]', '', 'ROS',
+           '', r'$\nu$ [spks/s]', 'SDM',
            colors, '(C)', label_prms)
+# Response-onset supersaturation
+plot_rates(fig.add_subplot(gs[1, 1]), [nu_ext_ros_a, nu_ext_ros_b],
+           [nu_ros_a, nu_ros_b], 50, 5,
+           '', '', 'ROS',
+           colors, '(D)', label_prms)
 # Mean-driven multisolution
-plot_rates(fig.add_subplot(gs[0, 3]),
+plot_rates(fig.add_subplot(gs[2, 0]),
            [nu_ext_mdm_a, nu_ext_mdm_b, nu_ext_mdm_c],
            [nu_mdm_a, nu_mdm_b, nu_mdm_c], 10, 50,
-           r'$\nu_X$ [spks/s]', '', 'MDM',
-           colors, '(D)', label_prms)
+           r'$\nu_X$ [spks/s]', r'$\nu$ [spks/s]', 'MDM',
+           colors, '(E)', label_prms)
 # Noise-driven multisolution
-plot_rates(fig.add_subplot(gs[0, 4]),
+plot_rates(fig.add_subplot(gs[2, 1]),
            [nu_ext_ndm_a, nu_ext_ndm_b, nu_ext_ndm_c, nu_ext_ndm_d],
            [nu_ndm_a, nu_ndm_b, nu_ndm_c, nu_ndm_d], 5, 10,
            r'$\nu_X$ [spks/s]', '', 'NDM',
-           colors, '(E)', label_prms)
+           colors, '(F)', label_prms)
 # save and show
 plt.savefig('sanzeni_fig8.png', dpi=300)
 plt.show()
--- lif_meanfield_tools/lif/_static.py
+++ lif_meanfield_tools/lif/_static.py
@@ -1,20 +1,40 @@
 from functools import partial
 import numpy as np
 import scipy.integrate as sint
+import scipy.optimize as sopt
 
 from .. import ureg
 
 
 def _firing_rate_integration(firing_rate_func, firing_rate_params,
-                             input_params):
+                             input_params, nu_0=None, fixpoint_method='ODE',
+                             eps_tol=1e-7, t_max_ODE=1000, maxiter_ODE=1000):
     """
     Solves the self-consistent eqs for firing rates, mean and std of input.
-    
-    Starts with a zero firing rate, calculates mean and std of the input and
-    then calculates the firing rate again. This iterative procedure is repeated
-    until the firing rates converge or an upper interation limit is reached.
+    If not specified otherwise, the intial guess nu_0 is a vanishing rate.
+
+    Currently, the following fixpoint_method are implemented:
+    * ODE
+    * LSQTSQ
+    ODE is a very good choice which finds stable fixed points even if the
+    initial guess is far from the fixed point. LSQTSQ also finds unstable
+    fixed points but needs a good initial guess.
+
+    ODE:
+    Solves the initial value problem
+      dnu / ds = - nu + firing_rate_func(nu)
+    with initial value nu_0 until the criterion for a self-consistent solution
+      max( abs(nu[t_max_ODE-1] - nu[t_max_ODE]) ) < eps_tol
+    is fulfilled. Raises an error if this does not happen within maxiter_ODE
+    iterations.
+
+    LSQTSQ:
+    Determines the minimum of
+      (nu - firing_rate_func(nu))^2
+    using least squares. Raises an error if the solution is a local minimum
+    with mean squared differnce above eps_tol.
     """
-    
+
     dimension = input_params['K'].shape[0]
 
     def get_rate_difference(_, nu, rate_func):
@@ -26,45 +46,60 @@ def _firing_rate_integration(firing_rate_func, firing_rate_params,
         # new std
         sigma = _std_input(nu=nu, **input_params)
         new_nu = rate_func(mu=mu, sigma=sigma, **firing_rate_params)
-        
+
         return -nu + new_nu
 
     get_rate_difference = partial(get_rate_difference,
                                   rate_func=firing_rate_func)
-    
-    # do iteration procedure, until stationary firing rates are found
-    eps_tol = 1e-12
-    t_max = 1000
-    maxiter = 1000
-    y0 = np.zeros(int(dimension))
-    for _ in range(maxiter):
-        sol = sint.solve_ivp(get_rate_difference, [0, t_max], y0,
-                             t_eval=[t_max - 1, t_max], method='LSODA')
-        assert sol.success is True
-        eps = max(np.abs(sol.y[:, 1] - sol.y[:, 0]))
-        if eps < eps_tol:
-            return sol.y[:, 1]
+
+    if nu_0 is None:
+        nu_0 = np.zeros(dimension)
+
+    if fixpoint_method == 'ODE':
+        # do iteration procedure, until stationary firing rates are found
+        for _ in range(maxiter_ODE):
+            sol = sint.solve_ivp(get_rate_difference, [0, t_max_ODE], nu_0,
+                                 t_eval=[t_max_ODE - 1, t_max_ODE],
+                                 method='LSODA')
+            assert sol.success is True
+            eps = max(np.abs(sol.y[:, 1] - sol.y[:, 0]))
+            if eps < eps_tol:
+                return sol.y[:, 1]
+            else:
+                nu_0 = sol.y[:, 1]
+        msg = f'Iteration failed to converge after {maxiter_ODE} steps. '
+        msg += f'Last maximum difference {eps:e}, desired {eps_tol:e}.'
+        raise RuntimeError(msg)
+    elif fixpoint_method == 'LSTSQ':
+        # search roots using least squares
+        get_rate_difference = partial(get_rate_difference, None)
+        res = sopt.least_squares(get_rate_difference, nu_0, bounds=(0, np.inf))
+        if res.cost/dimension < eps_tol:
+            return res.x
         else:
-            y0 = sol.y[:, 1]
-    msg = f'Rate iteration failed to converge after {maxiter} iterations. '
-    msg += f'Last maximum difference {eps:e}, desired {eps_tol:e}.'
-    raise RuntimeError(msg)
+            msg = 'Least squares converged in a local minimum. '
+            msg += f'Mean squared differences: {res.cost/dimension}.'
+            raise RuntimeError(msg)
+    else:
+        msg = f"The method '{fixpoint_method}' to determine the self-"
+        msg += "consistent fixpoint is not implemented."
+        raise NotImplementedError(msg)
 
 
 def mean_input(network, prefix):
     '''
     Calc mean inputs to populations as function of firing rates of populations.
-    
+
     See `delta.static.mean_input` or `exp.static.mean_input` for full
     documentation.
     '''
     return _input_calc(network, prefix, _mean_input)
-    
-    
+
+
 def std_input(network, prefix):
     '''
     Calc std of inputs to populations as function of firing rates.
-    
+
     See `delta.static.std_input` or `exp.static.std_input` for full
     documentation.
     '''
@@ -74,10 +109,10 @@ def std_input(network, prefix):
 def _input_calc(network, prefix, input_func):
     '''
     Helper function for input related calculations.
-    
+
     Checks the requirements for calculating input related quantities and calls
     the respective input function.
-    
+
     Parameters:
     -----------
     network: lif_meanfield_tools.create.Network object
@@ -98,9 +133,9 @@ def _input_calc(network, prefix, input_func):
         params = {key: network.network_params[key] for key in list_of_params}
     except KeyError as param:
         raise RuntimeError(f'You are missing {param} for this calculation.')
-    
+
     return input_func(rates, **params) * ureg.V
-    
+
 
 def _mean_input(nu, J, K, tau_m, J_ext, K_ext, nu_ext, tau_m_ext):
     """ Compute mean input without quantities. """
