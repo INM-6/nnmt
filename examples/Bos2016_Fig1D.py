5399f9430c29d0b538c9557080649e4655b29971
Simon Essink <essink@users.noreply.github.com>
2021-06-22 08:56:43 +0200

    Add scripts that reproduces Fig1D from Bos2016

---
+++ examples/Bos2016_Fig1D.py
@@ -0,0 +1,41 @@
+import nnmt
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py_wrapper.wrapper as h5
+
+
+plt.style.use('frontiers.mplstyle')
+
+# create network model microcircuit
+microcircuit = nnmt.models.Microcircuit(
+    '../tests/fixtures/integration/config/Bos2016_network_params.yaml')
+
+# calculate firing rates for exponentially shape post synaptic currents
+firing_rates = nnmt.lif.exp.firing_rates(microcircuit, method='taylor')
+
+print(f'Mean rates: {firing_rates}')
+
+fix_path = '../tests/fixtures/integration/data/'
+result = h5.load(fix_path + 'Bos2016_publicated_and_converted_data.h5')
+simulated_rates = result['fig_microcircuit']['rates_sim']*1000
+
+print(f'Mean simulated rates: {simulated_rates}')
+
+# one column figure, 85mm wide
+fig = plt.figure(figsize=(3.34646, 3.34646/2),
+                 constrained_layout=True)
+
+ax = fig.add_subplot(111)
+bars = ax.bar(np.arange(8), simulated_rates, align='center', color='grey',
+              edgecolor='black')
+
+for i in [0, 2, 4, 6]:
+    bars[i].set_color('black')
+    
+ax.scatter(np.arange(8), firing_rates, marker='X', color='r', s=50,
+           zorder=10)
+ax.set_xticks([0.5, 2.5, 4.5, 6.5])
+ax.set_xticklabels(['L2/3', 'L4', 'L5', 'L6'])
+ax.set_yticks([1, 3, 5, 7])
+ax.set_ylabel(r'$\bar{r}\,(1/s)$')
+plt.savefig('Bos2016_Fig1D.png')
