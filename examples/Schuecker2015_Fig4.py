3cf4ebcca0c7cd31107af0c649d70a2d13d9f591
Simon Essink <essink@users.noreply.github.com>
2021-07-29 17:22:15 +0200

    Improve Schuecker example * dimension into .yaml * deleted derivate at zero freq and replaced it by adding 0 to analysis freqs * improved parameter loading * added legend

--- examples/Schuecker2015_Fig4.py
+++ examples/Schuecker2015_Fig4.py
@@ -18,26 +18,23 @@ plt.style.use('frontiers.mplstyle')
 # The parameters used for calculation of the transfer functions 
 # in :cite:t:`schuecker2015` were gathered in a .yaml-File and are loaded here. 
 
-raw_params_with_units = nnmt.input_output.load_val_unit_dict_from_yaml(
+params = nnmt.input_output.load_val_unit_dict_from_yaml(
         '../tests/fixtures/integration/config/Schuecker2015_parameters.yaml')
-raw_params_with_units['dimension'] = 1
 
 # without converting to si
-params = raw_params_with_units.copy()
-nnmt.utils._strip_units(params)
-network_params = params
+network_params = params.copy()
+nnmt.utils._strip_units(network_params)
 
 # converting to si
-si_network_params = raw_params_with_units.copy()
-nnmt.utils._to_si_units(si_network_params)
-nnmt.utils._strip_units(si_network_params)
-si_network_params['dimension'] = 1
+si_network_params = params.copy()
+nnmt.utils._convert_to_si_and_strip_units(si_network_params)
 
 frequencies = np.logspace(
         si_network_params['f_start_exponent'],
         si_network_params['f_end_exponent'],
         si_network_params['n_freqs'])
-
+# add the zero frequency
+frequencies = np.insert(frequencies, 0, 0.0)
 omegas = 2 * np.pi * frequencies
 
 indices = [1,2]
@@ -60,17 +57,6 @@ for i, index in enumerate(indices):
         si_network_params['tau_r'],
         si_network_params['tau_s'])
     
-    # colored noise zero-frequency limit of transfer function
-    transfer_function_zero_freq = (
-        nnmt.lif.exp._derivative_of_firing_rates_wrt_mean_input(
-            si_network_params['V_reset'],
-            si_network_params['theta'],
-            si_network_params[f'mean_input_{index}'],
-            si_network_params[f'sigma_{index}'],
-            si_network_params['tau_m'],
-            si_network_params['tau_r'],
-            si_network_params['tau_s'])) / 1000
-    
     transfer_function = nnmt.lif.exp._transfer_function_shift(
         si_network_params[f'mean_input_{index}'],
         si_network_params[f'sigma_{index}'],
@@ -90,7 +76,6 @@ for i, index in enumerate(indices):
     # collect all results
     absolute_values.append(absolute_value)
     phases.append(phase)
-    transfer_function_zero_freqs.append(transfer_function_zero_freq)
     nu0_fbs.append(nu0_fb)
 
 # %% 
@@ -98,7 +83,6 @@ for i, index in enumerate(indices):
 pre_results = dict(
     absolute_values=absolute_values,
     phases=phases,
-    transfer_function_zero_freqs=transfer_function_zero_freqs,
     nu0_fbs=nu0_fbs)
     
 test_results = defaultdict(str)
@@ -111,8 +95,6 @@ for i, index in enumerate(indices):
         test_results['sigma'][sigma]['mu'][mu] = {
                 'absolute_value': pre_results['absolute_values'][i][:, j],
                 'phase': pre_results['phases'][i][:, j],
-                'transfer_function_zero_freq': \
-                    pre_results['transfer_function_zero_freqs'][i][j],
                 'nu0_fb': pre_results['nu0_fbs'][i][j]}
         
 # %%
@@ -133,6 +115,7 @@ for sigma in test_results['sigma'].keys():
         lw = 1
         markersize_cross = 4
         
+        # shift the zero frequency to be plotted on log-scale
         zero_freq = 0.06
         
         if sigma == 4.0:
@@ -140,20 +123,22 @@ for sigma in test_results['sigma'].keys():
         else:
             ls = '--'
 
-        axA.semilogx(frequencies,
-                        test_results['sigma'][sigma]['mu'][mu]['absolute_value'],
+        firing_rate = round(test_results['sigma'][sigma]['mu'][mu]['nu0_fb'])
+        # excluding zero frequency
+        axA.semilogx(frequencies[1:],
+                        test_results['sigma'][sigma]['mu'][mu]['absolute_value'][1:],
                         color=colors[i],
                         linestyle=ls,
-                        linewidth=lw,
-                        label=r'$\nu=$ {} Hz'.format(nu0_fb))
+                        linewidth=lw)
         axB.semilogx(frequencies,
                         test_results['sigma'][sigma]['mu'][mu]['phase'],
                         color=colors[i],
                         linestyle=ls,
                         linewidth=lw,
-                        label=r'$\mu=$ ' + str(mu))
+                        label=f'{mu} | {sigma}')
+        # just zero frequency
         axA.semilogx(zero_freq,
-                        test_results['sigma'][sigma]['mu'][mu]['transfer_function_zero_freq'],
+                        test_results['sigma'][sigma]['mu'][mu]['absolute_value'][0],
                         '+',
                         color=colors[i],
                         markersize=markersize_cross)
@@ -179,4 +164,8 @@ axA.xaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
 axB.xaxis.set_minor_locator(x_minor)
 axB.xaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
 
+axB.legend(title='$\mu [mV] | \sigma [mV]$', title_fontsize='xx-small',
+           handlelength=2, labelspacing=0.0)
+
 plt.savefig('Schuecker_Fig4.png')
+
