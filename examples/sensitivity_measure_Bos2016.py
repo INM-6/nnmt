60bf68ad082f3dce708458ea555ad798cba0c5e6
Simon Essink <essink@users.noreply.github.com>
2021-10-18 19:15:51 +0200

    Tweak sensitivity measure example * using fancy colorbar adjustment

--- examples/sensitivity_measure_Bos2016.py
+++ examples/sensitivity_measure_Bos2016.py
@@ -12,14 +12,16 @@ import nnmt
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
-
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+from mpl_toolkits.axes_grid1.inset_locator import InsetPosition
 
-def colorbar(mappable):
+
+def colorbar(mappable, cax=None):
     ax = mappable.axes
     fig = ax.figure
     divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
+    if cax==None:
+        cax = divider.append_axes("right", size="5%", pad=0.05)
     return fig.colorbar(mappable, cax=cax)
 
 plt.style.use('frontiers.mplstyle')
@@ -45,7 +47,11 @@ nnmt.network_properties.delay_dist_matrix(microcircuit)
 nnmt.lif.exp.effective_connectivity(microcircuit)
 
 sensitivity_dict = nnmt.lif.exp.sensitivity_measure_per_eigenmode(microcircuit)
-
+# %%
+import pickle
+# pickle.dump(sensitivity_dict, open('sensitivity_dict.pkl', 'wb'))
+sensitivity_dict = pickle.load(open('sensitivity_dict.pkl', 'rb'))
+# %%
 # print necessary entries of the sensitivity measure dictionary to see
 # which eigenvalues are needed to reproduce Fig.6 and Fig.7 of Bos 2016
 for i in range(8):
@@ -65,45 +71,61 @@ eigenvalue_to_plot_low = 6
 fig = plt.figure(figsize=(7.08661, 7.08661/2),
                  constrained_layout=True)
 grid_specification = gridspec.GridSpec(2, 2, figure=fig)
-colormap = 'coolwarm'
-labels = ['23E', '23I', '4E', '4I', '5E', '5I', '6E', '6I']
-plt.rcParams['xtick.labelsize'] = 'x-small'
-plt.rcParams['ytick.labelsize'] = 'x-small'
+# grid_specification.update(
+#     left=0.05, right=0.95, bottom=0.08, top=0.93, wspace=0.05, hspace=0.1)
 
-for ev, subpanel in zip(eigenvalues_to_plot_high, grid_specification):
-    gs = gridspec.GridSpecFromSubplotSpec(1, 2, 
-                                           width_ratios=[1, 1], 
-                                           subplot_spec=subpanel)
-    
+colormap = 'coolwarm'
+labels = ['2/3E', '2/3I', '4E', '4I', '5E', '5I', '6E', '6I']
+
+# too small for frontiers
+# plt.rcParams['xtick.labelsize'] = 'x-small'
+# plt.rcParams['ytick.labelsize'] = 'x-small'
+
+# set colorbar max and min
+z = 1
+label_prms = dict(x=-0.3, y=1.2, fontsize=10, fontweight='bold',
+                  va='top', ha='right')
+panel_labels = ['(A)', '(B)', '(C)', '(D)']
+
+for count, (ev, subpanel, panel_label) in enumerate(
+    zip(eigenvalues_to_plot_high, grid_specification, panel_labels)):
+
+    gs = gridspec.GridSpecFromSubplotSpec(1,3, 
+                                          height_ratios=[1],
+                                width_ratios=[1, 1, 0.2], 
+                                subplot_spec=subpanel)
+
     # sensitivity_measure_amplitude
     ax = fig.add_subplot(gs[0])
+    ax.text(s=panel_label, transform=ax.transAxes, **label_prms)
     
     frequency = sensitivity_dict[ev]['critical_frequency']
     projection_of_sensitivity_measure = sensitivity_dict[ev][
         'sensitivity_amp']
     
-    # obtain maximal absolute value
-    z = np.max(abs(projection_of_sensitivity_measure))
-    rounded_frequency = str(np.round(frequency,1))
+    rounded_frequency = str(int(np.round(frequency,0)))
 
     plot_title = r'$\mathbf{Z}^{\mathrm{amp}}(' + \
-        f'{rounded_frequency}' + r'\mathrm{Hz})$'
+        f'{rounded_frequency}' + r'\,\mathrm{Hz})$'
     ax.set_title(plot_title)
 
     heatmap = ax.imshow(projection_of_sensitivity_measure,
                         vmin=-z,
                         vmax=z,
-                        cmap=colormap)
-    colorbar(heatmap)
+                        cmap=colormap,
+                        aspect='equal')
+
+    
     if labels is not None:
         ax.set_xticks(np.arange(len(labels)))
         ax.set_yticks(np.arange(len(labels)))
-        ax.set_xticklabels(labels)
+        ax.set_xticklabels(labels, rotation=90)
         ax.set_yticklabels(labels)
 
     ax.set_xlabel('sources')
     ax.set_ylabel('targets')
     
+
     # sensitivity_measure_frequency
     ax = fig.add_subplot(gs[1])
     
@@ -111,32 +133,38 @@ for ev, subpanel in zip(eigenvalues_to_plot_high, grid_specification):
     projection_of_sensitivity_measure = sensitivity_dict[ev][
         'sensitivity_freq']
     
-    # obtain maximal absolute value
-    z = np.max(abs(projection_of_sensitivity_measure))
-    rounded_frequency = str(np.round(frequency,1))
+    rounded_frequency = str(int(np.round(frequency,0)))
 
     plot_title = r'$\mathbf{Z}^{\mathrm{freq}}(' + \
-        f'{rounded_frequency}' + r'\mathrm{Hz})$'
+        f'{rounded_frequency}' + r'\,\mathrm{Hz})$'
     ax.set_title(plot_title)
 
     heatmap = ax.imshow(projection_of_sensitivity_measure,
                         vmin=-z,
                         vmax=z,
-                        cmap=colormap)
-    colorbar(heatmap)
+                        cmap=colormap,
+                        aspect='equal')
+    
     if labels is not None:
         ax.set_xticks(np.arange(len(labels)))
         ax.set_yticks(np.arange(len(labels)))
-        ax.set_xticklabels(labels)
-        ax.set_yticklabels(labels)
+        ax.set_xticklabels(labels, rotation=90)
+        ax.set_yticklabels([])
 
     ax.set_xlabel('sources')
-    ax.set_ylabel('targets')
-
-fig.set_constrained_layout_pads(w_pad=2 / 72, h_pad=2 / 72, hspace=0.1,
-                                wspace=0.1)    
+    # ax.set_ylabel('targets')
+        
+    colorbar_ax = fig.add_subplot(gs[2])
     
-plt.savefig('figures/sensitivity_measure_high_gamma_Bos2016.pdf', bbox_inches='tight')
+    colorbar_width = 0.1
+    ip = InsetPosition(ax, [1.05,0,colorbar_width,1]) 
+    colorbar_ax.set_axes_locator(ip)
+    colorbar(heatmap, cax=colorbar_ax)
+
+fig.set_constrained_layout_pads(w_pad=0, h_pad=0,
+                                hspace=0.1, wspace=0.1)    
+    
+plt.savefig('figures/sensitivity_measure_high_gamma_Bos2016.eps')
 
 # %%
 # Plotting: Sensitivity Measure corresponding to low frequencies (Fig. 7)
@@ -159,10 +187,10 @@ projection_of_sensitivity_measure = sensitivity_dict[ev][
 
 # obtain maximal absolute value
 z = np.max(abs(projection_of_sensitivity_measure))
-rounded_frequency = str(np.round(frequency,1))
+rounded_frequency = str(np.round(frequency,2))
 
 plot_title = r'$\mathbf{Z}^{\mathrm{amp}}(' + \
-        f'{rounded_frequency}' + r'\mathrm{Hz})$'
+        f'{rounded_frequency}' + r'\,\mathrm{Hz})$'
 ax.set_title(plot_title)
 
 heatmap = ax.imshow(projection_of_sensitivity_measure,
@@ -180,3 +208,4 @@ ax.set_xlabel('sources')
 ax.set_ylabel('targets')
 
 plt.savefig('figures/sensitivity_measure_low_gamma_Bos2016.pdf', bbox_inches='tight')
+# %%
