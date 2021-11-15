18345819c6da1e00cc2170e1fadae3fd84286cb6
Moritz Layer <m.layer@fz-juelich.de>
2021-11-15 11:22:57 +0100

    Add test for `_fit_transfer_function`

--- tests/unit/lif/test_lif.py
+++ tests/unit/lif/test_lif.py
@@ -8,13 +8,13 @@ from numpy.testing import (
     assert_array_equal,
     )
 
-import nnmt.lif._general as static
+import nnmt.lif._general as general
 from nnmt import ureg
 
 
 class Test_mean_input:
 
-    func = staticmethod(static._mean_input)
+    func = staticmethod(general._mean_input)
     fixtures = 'lif_mean_input.h5'
 
     def test_correct_output(self, unit_fixtures):
@@ -26,7 +26,7 @@ class Test_mean_input:
 
 class Test_std_input:
 
-    func = staticmethod(static._std_input)
+    func = staticmethod(general._std_input)
     fixtures = 'lif_std_input.h5'
 
     def test_correct_output(self, unit_fixtures):
@@ -39,7 +39,7 @@ class Test_std_input:
 
 class Test_input_function_wrapper:
 
-    func = staticmethod(static._input_calc)
+    func = staticmethod(general._input_calc)
 
     def mock_mean_input(self, mocker):
         mocker.patch('nnmt.lif._general._mean_input',
@@ -51,7 +51,7 @@ class Test_input_function_wrapper:
         self.mock_mean_input(mocker)
         empty_network.results['test.firing_rates'] = np.array([1]) * ureg.Hz
         with pytest.raises(RuntimeError):
-            self.func(empty_network, 'test.', static._mean_input)
+            self.func(empty_network, 'test.', general._mean_input)
 
     def test_raise_exception_if_no_firing_rate_available(self, mocker,
                                                          empty_network):
@@ -63,7 +63,7 @@ class Test_input_function_wrapper:
         empty_network.network_params['J_ext'] = 1
         empty_network.network_params['nu_ext'] = 1
         with pytest.raises(RuntimeError):
-            self.func(empty_network, 'test.', static._mean_input)
+            self.func(empty_network, 'test.', general._mean_input)
 
     def test_returns_unit(self, mocker, empty_network):
         self.mock_mean_input(mocker)
@@ -74,6 +74,28 @@ class Test_input_function_wrapper:
         empty_network.network_params['J_ext'] = 1
         empty_network.network_params['nu_ext'] = 1
         empty_network.results['test.firing_rates'] = np.array([1]) * ureg.Hz
-        result = self.func(empty_network, 'test.', static._mean_input)
+        result = self.func(empty_network, 'test.', general._mean_input)
         assert isinstance(result, ureg.Quantity)
 
+
+class Test_fit_transfer_function:
+
+    func = staticmethod(general._fit_transfer_function)
+
+    def test_fits_low_pass_filter_correctly(self):
+
+        def low_pass_filter(omega, tau, h0):
+            return h0 / (1. + 1j * omega * tau)
+
+        tau = 0.01
+        h0 = 0.8
+        omegas = np.linspace(0, 500, 2000)
+        transfunc = low_pass_filter(omegas, tau, h0)[np.newaxis].T
+
+        transfunc_fit, tau_fit, h0_fit, fit_error = (
+            general._fit_transfer_function(transfunc, omegas))
+
+        assert tau_fit[0] == tau
+        assert h0_fit[0] == h0
+        assert_array_equal(transfunc_fit, transfunc)
+        assert fit_error == 0
