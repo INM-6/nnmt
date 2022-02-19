53f589fb9712b48d55100c25d3e7cb35ddc1253a
Moritz Layer <m.layer@fz-juelich.de>
2022-02-19 17:17:44 +0100

    Update version number and copyright date

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='nnmt',
-      version='1.0.0',
+      version='1.0.1',
       description='Neuronal Network Meanfield Toolbox',
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
