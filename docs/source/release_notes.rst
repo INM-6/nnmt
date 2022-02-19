bb067ae58a5f8f5f1666404839e4e77e2f49cebd
Moritz Layer <m.layer@fz-juelich.de>
2022-02-19 17:53:55 +0100

    Add approximations and assumptions to docstrings

--- docs/source/release_notes.rst
+++ docs/source/release_notes.rst
@@ -5,6 +5,15 @@ Release notes
 =============
 
 **********
+NNMT 1.0.1
+**********
+
+- Deepcopy parameter dictionaries on instantiation of network model. Otherwise
+dictionary items can change unwantedly if netork parameters are changed.
+- Fix typos in docstrings.
+- Add approximations and assumptions to docstrings.
+
+**********
 NNMT 1.0.0
 **********
 
--- nnmt/lif/exp.py
+++ nnmt/lif/exp.py
@@ -164,10 +164,10 @@ def _firing_rates(J, K, V_0_rel, V_th_rel, tau_m, tau_r, tau_s, J_ext, K_ext,
     Calculates the stationary firing rate of a neuron with synaptic filter of
     time constant tau_s driven by Gaussian noise with mean mu and standard
     deviation sigma based on :cite:t:`fourcaud2002`, using either a shift of
-    the integration boundaries in the white noise Siegert formula, as derived
-    in :cite:t:`schuecker2015` (default), or a Taylor expansion around
+    the integration boundaries in the white noise Siegert formula, calling
+    :func:`nnmt.lif.exp._firing_rate_shift`, or a Taylor expansion around
     :math:`k = \sqrt{\\tau_\mathrm{s}/\\tau_\mathrm{m}}` of Eq. 4.33 in
-    :cite:t:`fourcaud2002`.
+    :cite:t:`fourcaud2002`, calling :func:`nnmt.lif.exp._firing_rate_taylor`.
 
     Parameters
     ----------
@@ -233,10 +233,15 @@ def _firing_rates(J, K, V_0_rel, V_th_rel, tau_m, tau_r, tau_s, J_ext, K_ext,
 @_check_k_in_fast_synaptic_regime
 def _firing_rate_shift(V_0_rel, V_th_rel, mu, sigma, tau_m, tau_r, tau_s):
     """
-    Calculates stationary firing rates including synaptic filtering.
+    Calculates stationary mean firing rates including synaptic filtering.
 
-    Based on :cite:t:`fourcaud2002`, using shift of the integration boundaries
-    in the white noise Siegert formula, as derived in :cite:t:`schuecker2015`.
+    Based on the equation after Eq. 4.33 in :cite:t:`fourcaud2002`, using shift
+    of the integration boundaries in the white noise Siegert formula.
+
+    **Assumptions and approximations**:
+
+    - Diffusion approximation
+    - Fast synapses: :math:`\sqrt{\\tau_\mathrm{s} / \\tau_\mathrm{m}} \ll 1`
 
     Parameters
     ----------
@@ -274,12 +279,17 @@ def _firing_rate_shift(V_0_rel, V_th_rel, mu, sigma, tau_m, tau_r, tau_s):
 @_check_k_in_fast_synaptic_regime
 def _firing_rate_taylor(V_0_rel, V_th_rel, mu, sigma, tau_m, tau_r, tau_s):
     """
-    Calcs stationary firing rates for exp PSCs using a Taylor expansion.
+    Calcs stationary mean firing rates including synaptic filtering.
 
     Calculates the stationary firing rate of a neuron with synaptic filter of
     time constant tau_s driven by Gaussian noise with mean mu and standard
     deviation sigma, using Eq. 4.33 in :cite:t:`fourcaud2002` with Taylor
     expansion around :math:`k = \sqrt{\\tau_\mathrm{s}/\\tau_\mathrm{m}}`.
+
+    **Assumptions and approximations**:
+
+    - Diffusion approximation
+    - Fast synapses: :math:`\sqrt{\\tau_\mathrm{s} / \\tau_\mathrm{m}} \ll 1`
 
     Parameters
     ----------
@@ -668,15 +678,23 @@ def _transfer_function_shift(mu, sigma, tau_m, tau_s, tau_r, V_th_rel,
     """
     Calcs value of transfer func for one population at given frequency omega.
 
-    Calculates transfer function according to :math:`\\tilde{n}` in
-    :cite:t:`schuecker2015`. The expression is to first order equivalent to
-    :func:`nnmt.lif.exp._transfer_function_taylor`. Since the underlying theory
-    is correct to first order, the two expressions are exchangeable.
+    Calculates transfer function based on :math:`\\tilde{n}` in
+    :cite:t:`schuecker2015`. The expression is to first order in
+    :math:`\sqrt{\\tau_\mathrm{s} / \\tau_\mathrm{m}}` equivalent to
+    :func:`nnmt.lif.exp._transfer_function_taylor`.
 
-    The difference here is that the linear response of the system is considered
-    with respect to a perturbation of the input to the current I, leading to an
-    additional low pass filtering 1/(1+i omega tau_s). Compare with the second
-    equation of Eq. 18 and the text below Eq. 29.
+    The difference to the equation in :cite:t:`schuecker2015` is that the
+    linear response of the system is considered with respect to a perturbation
+    of the input to the current I, leading to an additional synaptic low pass
+    filter 1/(1+i omega tau_s). Compare with the second equation of Eq. 18 and
+    the text below Eq. 29.
+
+    **Assumptions and approximations**:
+
+    - Diffusion approximation
+    - Linear response theory
+    - Fast synapses: :math:`\sqrt{\\tau_\mathrm{s} / \\tau_\mathrm{m}} \ll 1`
+    - Low frequencies: :math:`\omega\sqrt{\\tau_\mathrm{m} \\tau_\mathrm{s}} \ll 1`
 
     Parameters
     ----------
@@ -759,8 +777,16 @@ def _transfer_function_taylor(mu, sigma, tau_m, tau_s, tau_r, V_th_rel,
 
     The difference here is that the linear response of the system is considered
     with respect to a perturbation of the input to the current I, leading to an
-    additional low pass filtering 1/(1+i omega tau_s).
-    Compare with the second equation of Eq. 18 and the text below Eq. 29.
+    additional synaptic low pass filter 1/(1+i omega tau_s). Compare with the
+    second equation of Eq. 18 and the text below Eq. 29.
+
+    **Assumptions and approximations**:
+
+    - Diffusion approximation
+    - Linear response theory
+    - Fast synapses: :math:`\sqrt{\\tau_\mathrm{s} / \\tau_\mathrm{m}} \ll 1`
+    - Low frequencies: :math:`\omega\sqrt{\\tau_\mathrm{m} \\tau_\mathrm{s}}
+      \ll 1`
 
     Parameters
     ----------
@@ -970,6 +996,11 @@ def _derivative_of_firing_rates_wrt_mean_input(V_0_rel, V_th_rel, mu, sigma,
 
     See Appendix B in :cite:t:`schuecker2014`.
 
+    **Assumptions and approximations**:
+
+    - Diffusion approximation
+    - Fast synapses: :math:`\sqrt{\\tau_\mathrm{s} / \\tau_\mathrm{m}} \ll 1`
+
     Parameters
     ----------
     tau_m : [float | np.ndarray]
@@ -1067,6 +1098,11 @@ def _derivative_of_firing_rates_wrt_input_rate(
     Derivative of the stationary firing rates with respect to input rate.
 
     See Eq. A.3 in Appendix A of :cite:t:`helias2013`.
+
+    **Assumptions and approximations**:
+
+    - Diffusion approximation
+    - Fast synapses: :math:`\sqrt{\\tau_\mathrm{s} / \\tau_\mathrm{m}} \ll 1`
 
     Parameters
     ----------
